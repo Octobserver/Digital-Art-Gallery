2a9050419483e714ee012d791fbbbfc60062ce94
Weina Dai <weinadai@Weinas-Air.lan>
2023-08-26 02:15:58 -0400

    Make portfolio page responsive

---
+++ src/helper/PortfolioItemList.js
@@ -0,0 +1,33 @@
+import character_full from "../assets/character-art.jpg";
+import character_bust from "../assets/character-art2.jpg";
+import character_design from "../assets/character-design.jpg";
+import pet_portrait from "../assets/pet-portrait.jpg";
+import still_life from "../assets/summer.jpg";
+
+export const PortfolioItemList = [
+  {
+    imageUrl: character_full,
+    title: "Prophet",
+    tag: "Character Art (Full-Body)",
+  },
+  {
+    imageUrl: character_bust,
+    title: "Artist",
+    tag: "Character Art (Bust)",
+  },
+  {
+    imageUrl: character_design,
+    title: "Blue Jay",
+    tag: "Character Design (Color Rough)",
+  },
+  {
+    imageUrl: pet_portrait,
+    title: "My Furball",
+    tag: "Pet Portrait",
+  },
+  {
+    imageUrl: still_life,
+    title: "The Watery Summer",
+    tag: "Still Life",
+  },
+];
--- src/pages/MyWork.jsx
+++ src/pages/MyWork.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import {  Box  } from "@chakra-ui/react";
 import PortfolioItem from '../components/PortfolioItem';
-import summer from '../assets/summer.jpg';
+import {PortfolioItemList} from '../helper/PortfolioItemList';
+import '../styles/MyWork.css';
 
 function MyWork() {
   return (
-    <div className="projects">
+    <div className="my-work-wrapper">
       <h1> Portfolio</h1>
-      <div className="artWorkList">
-        <PortfolioItem imageUrl={summer} title="The Watery Summer" tag="Still Life"/>
-      </div>
+      <Box
+        padding={4}
+        w="100%"
+        maxW="1000px"
+        mx="auto"
+        bg="gray.800"
+        sx={{ columnCount: [1, 2, 3], columnGap: "8px" }}
+      >
+        {PortfolioItemList.map((item, idx) => {
+          return (
+            <PortfolioItem id={idx} imageUrl={item.imageUrl} title={item.title} tag={item.tag}/>
+          );
+        })}
+      </Box>
     </div>
   );
 }
